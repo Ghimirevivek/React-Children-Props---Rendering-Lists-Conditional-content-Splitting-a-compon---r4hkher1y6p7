668082101dc5d9f37f48459a6283a0f28c3b378e
Ghimirevivek <108508449+Ghimirevivek@users.noreply.github.com>
2023-03-07 15:53:53 +0530

    Update ChildComponent.js

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const ChildComponent = () => {
-//code here
+const ChildComponent = ({ children }) => {
   return (
     <div id="child">
       Find The Square <br/>
-     
+      {children}
     </div>
-  )
+  );
 }
-
 
 export default ChildComponent;
